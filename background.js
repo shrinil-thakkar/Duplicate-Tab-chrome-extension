1123e47d63f2d89eb6b97386b12ecc3a6d34d7ac
shrinilthakkar <shrinil@moengage.com>
2018-06-13 14:12:46 +0530

    removed unwanted files

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-//chrome.runtime.onInstalled.addListener(function() {
-//  chrome.storage.sync.set({color: '#333'}, function() {
-//    chrome.tabs.create({"url": "http://google.com"});
-//    console.log("The color is gre.");
-//  });
-//});
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
--- popup.js
+++
@@ -1,6 +0,0 @@
-let changeColor = document.getElementById('changeColor');
-
-  chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = 'red';
-    changeColor.setAttribute('value', 'red');
-  });
